45500cb57f2c93157c60b25d5dd4459ec3ca9173
kennedymakokha <katchibo2@gmail.com>
2025-04-16 05:23:10 +0300

    setup and configure tailwind

---
+++ src/screens/splashsreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Splashsreen = () => {
+    return (
+        <View className='flex-1 bg-blue-800'>
+            <Text style={{color:"red"}}>Splashsreen</Text>
+        </View>
+    )
+}
+
+export default Splashsreen
+
